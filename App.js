6ab6226c62c0afad188f6fdc832ba87ac47f1221
Erfan91 <sadidhadix@gmail.com>
2022-04-06 16:49:59 +0200

    'chapitre2'

--- App.js
+++ App.js
@@ -1,15 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
+import { useState, useEffect } from 'react';
+export default function App(props) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View >
+
+      <Text>whast popping buooouuyyuyuy</Text>
+      <Ist />
     </View>
+
   );
 }
+
+const Ist = () => {
+  const [list, setList] = useState([])
+  const [loading, stLoading] = useState(true)
+  const url = 'https://restcountries.com/v3.1/all';
+
+
+
+
+  useEffect(() => {
+    fetch(url).then((result) => result.json())
+      .then((json) => setList(json))
+      .catch((error) => alert('Error'))
+      .finally(() => stLoading(false))
+    console.log(list)
+  }, [])
+
+  return (
+    <View >
+      <FlatList
+        data={list}
+        renderItem={({ item }) => (
+          <View>
+            <Text style={styles.text}>{item.name.common}</Text>
+            <Text style={styles.text1}>{item.capital}</Text>
+          </View>
+        )} />
+    </View>
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,5 +50,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+   
   },
+  text: {
+    color: 'black',
+    backgroundColor: 'violet',
+    width: 400,
+    fontSize:33
+  },
+  text1:{
+    color:'violet',
+    backgroundColor:'black',
+    fontSize:29,
+  
+    
+  }
 });
---
+++ compnent/List.jsx
@@ -0,0 +1,20 @@
+// import axios from 'axios';
+// import React from 'react'
+// import { useState, useEffect } from 'react'
+// import { View } from 'react-native';
+// const List = (props) => {
+//     const [List, setList] = useState([]);
+//     const url = 'http://restcountries.eu/rest/v2/all';
+//     useEffect(()=>{
+//         axios(url).then(result =>{
+//             console.log(result)
+//         })
+//     },[])
+//   return (
+//     <View>
+//         <Text>hi</Text>
+//     </View>
+//   )
+// }
+
+// export default List
